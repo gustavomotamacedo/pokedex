f41a07f0a7f8eeb97e97338436e642b42a1aa209
gustavomotamacedo <mota.macedo05@gmail.com>
2023-08-21 14:36:23 -0300

    dinamizando tipos

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,12 +2,12 @@ const pokemonOl = document.querySelector(".pokemons")
 
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon">
+        <li class="pokemon ${pokemon.type}">
             <span class="number">#${pokemon.order}</span>
             <span class="name">${pokemon.name}</span>
             <div class="detail">
                 <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
                 </ol>
                 <img src="${pokemon.sprite}" alt="${pokemon.name}">
             </div>
